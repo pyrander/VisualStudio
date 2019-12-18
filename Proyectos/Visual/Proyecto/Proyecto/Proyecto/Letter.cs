94d7b30f6a026e9e4c289ecf6586e2e33e44f20a
Alex <aguione@gmail.com>
2019-12-17 19:30:31 -0500

    proyecto avance

---
+++ Proyectos/Visual/Proyecto/Proyecto/Proyecto/Letter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto {
+    class Letter {
+        private string symbol = "";
+
+        public Letter (string symbol)
+        {
+            this.symbol = symbol;
+        }
+
+        public string GetSymbol ()
+        {
+            return symbol;
+        }
+    }
+}
