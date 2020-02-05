105d76ac9099ecd45de4bf5388ea7fdad08897bf
unknown <aguione@gmail.com>
2020-02-04 21:53:39 -0500

    avance patrulla

--- Clases/TopDown/TopDown2D/Assets/Scripts/MovePointClick.cs
+++ Clases/TopDown/TopDown2D/Assets/Scripts/MovePointClick.cs
@@ -2,17 +2,71 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovePointClick : MonoBehaviour
+namespace Topdown
 {
-    // Start is called before the first frame update
-    void Start()
+    public class MovePointClick : MonoBehaviour
     {
-        
-    }
+        public enum State
+        {
+            idle, move
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private Camera mainCamera;
+        [SerializeField]
+        private State state = State.idle;
+
+        public float speed = 1f;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            mainCamera = Camera.main;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetMouseButtonDown(0) &&
+                state == State.idle)
+            {
+                Vector3 pos =
+                mainCamera.ScreenToWorldPoint(
+                    Input.mousePosition
+                );
+                pos.z = 0;
+
+                StopAllCoroutines();
+                StartCoroutine(Move(pos));
+            }
+        }
+
+        IEnumerator Move(Vector3 target)
+        {
+            state = State.move;
+            float distance =
+                Vector3.Distance(
+                    transform.position,
+                    target
+                );
+
+
+            Vector3 direction = target - transform.position;
+            direction.Normalize();
+
+            while (distance > 0.05f)
+            {
+                transform.position +=
+                    direction * speed * Time.deltaTime;
+
+                distance = Vector3.Distance(
+                    transform.position,
+                    target
+                );
+                yield return new WaitForEndOfFrame();
+            }
+
+            transform.position = target;
+            state = State.idle;
+        }
     }
 }
--- Clases/TopDown/TopDown2D/Assets/Scripts/Patrol.cs
+++ Clases/TopDown/TopDown2D/Assets/Scripts/Patrol.cs
@@ -1,18 +1,109 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-public class Patrol : MonoBehaviour
+namespace Topdown
 {
-    // Start is called before the first frame update
-    void Start()
+    public class Patrol : MonoBehaviour
     {
-        
-    }
+        float t = 0f;
+        Vector3 origin;
+        Vector3 startPos;
+        Vector3 endPos;
+        Vector3 point;
+        int currentIndex;
+        public Vector2[] points;
+        public float speed = 1f;
+        public ActionArea player;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
-    
+        // Start is called before the first frame update
+        void Start()
+        {
+            currentIndex = 0;
+            point = points[currentIndex];
+            origin = transform.position;
+            startPos = origin;
+            endPos = origin + point;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+            transform.position = Vector3.Lerp(startPos, endPos, t);
+
+            t += Time.deltaTime * speed;
+
+            if (Vector3.Distance(transform.position, player.transform.position) <= player.radius)
+            {
+                speed *= 2f;
+            }
+            else
+            {
+                float distance = Vector3.Distance(startPos, endPos);
+                speed = 2f / distance;
+            }
+
+
+            if (t >= 1f)
+            {
+                startPos = endPos;
+                currentIndex++;
+
+                if (currentIndex >= points.Length)
+                {
+                    currentIndex = -1;
+                    point = Vector3.zero;
+                }
+                else
+                {
+                    point = points[currentIndex];
+                }
+
+                endPos = origin + point;
+
+                float distance = Vector3.Distance(startPos, endPos);
+                speed = 2f / distance;
+
+                t = 0;
+            }
+
+
+        }
+
+        private void OnDrawGizmos()
+        {
+            if (!Application.isPlaying && origin != transform.position)
+            {
+                origin = transform.position;
+            }
+                
+            Vector3 point = points[0];
+            Debug.DrawLine(
+                origin,
+                origin + point,
+                Color.cyan);
+
+            for (int i = 1; i < points.Length; i++)
+            {
+                point = points[i - 1];
+                Vector3 start = origin + point;
+
+                point = points[i];
+                Vector3 end = origin + point;
+                Debug.DrawLine(
+                    start,
+                    end,
+                    Color.cyan);
+            }
+
+            point = points[points.Length - 1];
+            Debug.DrawLine(
+                origin + point,
+                origin,
+                Color.cyan);
+        }
+
+    }
 }
